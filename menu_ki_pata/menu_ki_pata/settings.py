b6c8c1a283a702f0446e17196f903a05b5f9b330
Soham <141822047+XohamOG@users.noreply.github.com>
2024-10-09 22:07:30 +0530

    first innitttttt

--- menu_ki_pata/menu_ki_pata/settings.py
+++ menu_ki_pata/menu_ki_pata/settings.py
@@ -54,15 +54,15 @@ ROOT_URLCONF = "menu_ki_pata.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
